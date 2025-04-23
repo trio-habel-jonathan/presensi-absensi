9ba29047a6af2502c88df6c77ad0cff6f635d9a0
Habib <heandsome083@gmail.com>
2025-04-23 17:03:35 +0700

    update

--- resources/views/admin/izin/index.blade.php
+++ resources/views/admin/izin/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.navbars.auth.topnav', ['title' => 'Manajemen Izin'])
 <div class="row mt-4 mx-4">
     <div class="col-12">
         <div class="card mb-4">
--- resources/views/admin/profil_pegawai/edit.blade.php
+++ resources/views/admin/profil_pegawai/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Edit Employee'])
+    {{-- @include('layouts.navbars.auth.topnav', ['title' => 'Edit Employee']) --}}
     <div class="row mt-4 mx-4">
         <div class="col-12">
             <div class="card mb-4">
